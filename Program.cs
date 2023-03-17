684a476ff7b3b205c0f37bf08b2df5abe9890afe
Bitnik <kukiyshka@gmail.com>
2023-03-17 21:13:39 +0300

    Formatting as a method and add comments

--- Program.cs
+++ Program.cs
@@ -2,12 +2,21 @@
 // либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте
 // выполнения алгоритма. 
 
-Console.Clear();
-Console.Write("Введите слова(символы) строки через пробел: ");
-string row = Console.ReadLine()!;
-//далее создаем массив из подстрок, разделенных пробелами и удаляем пустые (только с пробелами) подстроки
-string[] Row = row.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+void ArrayElements3Simbol(string[] Row) // метод записи в массив элементов с длиной <=3 и вывода в консоль
+{
+    string[] RowLength3 = Array.FindAll(Row, person => person.Length <= 3);
+    foreach (var person in RowLength3) ;
+    Console.WriteLine("Массив из строк с длиной меньше или равно 3 символам: " + String.Join(" ", RowLength3));
+}
 
-string[] RowLength3 = Array.FindAll(Row, person => person.Length <= 3);
-foreach (var person in RowLength3) ;
-Console.WriteLine("Массив из строк с длиной меньше или равно 3 символам: " + String.Join(" ", RowLength3));
+void Main()      // метод ввода элементов строки и записи в массив этих элементов
+{
+    Console.Clear();
+    Console.Write("Введите слова(символы) строки через пробел: ");
+    string row = Console.ReadLine()!;
+    // создаем массив из подстрок, разделенных пробелами и удаляем пустые (только с пробелами) подстроки
+    string[] Row = row.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+    ArrayElements3Simbol(Row);
+}
+
+Main();
