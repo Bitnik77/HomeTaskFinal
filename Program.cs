7eab85f4533d8390415b44c31908ff89b78a8c85
Bitnik <kukiyshka@gmail.com>
2023-03-17 17:18:46 +0300

    Enter row and view result

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше
+// либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте
+// выполнения алгоритма. 
+
+Console.Clear();
+Console.Write("Введите слова(символы) строки через пробел: ");
+string row = Console.ReadLine()!;
+//далее создаем массив из подстрок, разделенных пробелами и удаляем пустые (только с пробелами) подстроки
+string[] Row = row.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine(String.Join(" ", Row));
